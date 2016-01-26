49d2245484d013446363a77ac9cea1327389a8a3
Damien DeVille <damien@ddeville.me>
2016-01-26 21:33:14 +0000

    Delete sdk

--- sdk/MacOSX10.11.sdk/System/Library/Frameworks/CryptoTokenKit.framework/Versions/A/Headers/CryptoTokenKit.h
+++
@@ -1,9 +0,0 @@
-//
-//  CryptoTokenKit.h
-//  Copyright (c) 2014-2015 Apple. All rights reserved.
-//
-
-#import <CryptoTokenKit/TKError.h>
-#import <CryptoTokenKit/TKSmartCard.h>
-#import <CryptoTokenKit/TKSmartCardATR.h>
-
--- sdk/MacOSX10.11.sdk/System/Library/Frameworks/vecLib.framework/Versions/A/Headers/vecLib.h
+++
@@ -1,1 +0,0 @@
-#error "The vecLib framework is deprecated. Please include <Accelerate/Accelerate.h> instead of <vecLib/vecLib.h> and link to the Accelerate framework instead of the vecLib framework."
